d350d0bc607337c51b7624496435f8c2232d59ea
Thomas Stubblefield <tstubblefield487@gmail.com>
2021-12-01 19:30:43 -0500

    Updated Timer

--- Stretch+Paws/DetailView.swift
+++ Stretch+Paws/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let pose: Pose
+    @State private var timerOpen = false
     var body: some View {
         ZStack {
             Color("Secondary").ignoresSafeArea()
@@ -42,6 +43,11 @@ struct DetailView: View {
                     }
                 }.padding(.horizontal, 20)
             }
+            TimerPanelView(timerOpen: $timerOpen)
+            
+        }
+        .onTapGesture {
+            timerOpen = false
         }
     }
 }
@@ -56,5 +62,67 @@ struct DetailView_Previews: PreviewProvider {
             "Did someone say dog? Can't we call this a downward-facing cat instead? It's OK – this is a friendly dog, it's not interested in chasing cats. In fact, Downward-Facing Dog is the lynchpin of a yoga asana practice: if you're going to befriend with any of these poses, make sure it's this canine classic.",
             steps: ["From a kneeling position, place your hands shoulder-distance apart and spread your fingers.", "Tuck your toes and lift your hips up towards the ceiling so you create an inverted V shape.", "Balance the weight between hands and feet and think about tilting your tailbone up towards the ceiling.","Send your gaze towards your feet and breath!"],
             topTip: "Bend your knees in order to create more length through the spine." ))
+    }
+}
+
+struct TimerPanelView: View {
+    
+    @StateObject var yogaTimer = YogaTimer()
+    
+    @Binding var timerOpen: Bool
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack {
+                timerOpen ? AnyView(TimerOpenView(yogaTimer: yogaTimer)) : AnyView(TimerClosedView())
+            }   .frame(maxWidth: .infinity, maxHeight: timerOpen ? 400 : 80)
+                .background(Color("Highlight"))
+                .cornerRadius(6)
+                .foregroundColor(Color("Secondary"))
+                .onTapGesture {
+                    timerOpen.toggle()
+                }
+        }.ignoresSafeArea()
+    }
+}
+
+struct TimerOpenView: View {
+    
+    @ObservedObject var yogaTimer: YogaTimer
+    
+    var body: some View {
+        VStack {
+            Text("Hold that pose")
+                .fontWeight(.medium)
+            Spacer()
+            Text("Try staying in this pose for 30 seconds. If you need to come out sooner, that's ok.")
+                .multilineTextAlignment(.center)
+            Spacer()
+            Text(yogaTimer.timerDuration < 10 ? "00:0\(yogaTimer.timerDuration)" : "00:\(yogaTimer.timerDuration)")
+                .font(.system(size: 96))
+            Spacer()
+            Button{
+                yogaTimer.startTimer()
+            } label: {
+                Text("Start the timer")
+                    .fontWeight(.medium)
+            }
+            .frame(width: 300, height: 50, alignment: .center)
+                .background(Color("Secondary"))
+                .foregroundColor(Color("Primary"))
+                .cornerRadius(60)
+        Spacer()
+        }
+        
+        .padding(30)
+    }
+}
+
+struct TimerClosedView: View {
+    var body: some View {
+        Text("Try it out")
+            .fontWeight(.medium)
+            .padding(20)
+        Spacer()
     }
 }
---
+++ Stretch+Paws/YogaTimer.swift
@@ -0,0 +1,52 @@
+//
+//  YogaTimer.swift
+//  Stretch+Paws
+//
+//  Created by Thomas Stubblefield on 11/30/21.
+//
+
+import Foundation
+
+// What kind of data type? Class or struct? Class because it is only one timer
+// Where does the timer need to be shared?
+
+class YogaTimer: ObservableObject {
+    @Published var timerActive = false
+    @Published var timerPaused = false
+    @Published var timerEnded = false
+    @Published var timerDuration = 30
+    var yogaTimer = Timer()
+    // Timer Functionality
+    // Start the timer
+    func startTimer() {
+        timerActive = true
+        yogaTimer = Timer.init(timeInterval: 1, repeats: true, block: { Timer in
+            // Remove a second from the timer duration & loop
+            self.timerDuration -= 1
+            // If the timer gets to 0, stop the timer
+            if self.timerDuration == 0 {
+                self.stopTimer()
+            }
+        })
+        
+    }
+    // Pause the timer
+    func pauseTimer() {
+        timerActive = false
+        timerPaused = true
+        yogaTimer.invalidate()
+    }
+    // End the timer
+    func stopTimer() {
+        // Play a sound
+        timerEnded = true
+        timerActive = false
+        yogaTimer.invalidate()
+        timerDuration = 30
+    }
+    // Play a sound
+    func playSound() {
+        // Play audio file
+    }
+    // Countdown
+}
